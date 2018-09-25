6d32d3f2e7d43e3e6da8975e278ee448c57466cc
zhang <zhangjuming2010@126.com>
2018-09-25 21:53:06 +0800

    集成“iview-admin-2.0(基于Beta4,后台接口只需提供菜单JSON，即可转为标准路由JOSN)”

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,7 @@
 import Mock from 'mockjs'
-import { login, logout, getUserInfo } from './login'
-import { getTableData, getDragList } from './data'
+import { getUserInfo, getUserSysMenu, login, logout } from './login'
+import { getDragList, getTableData } from './data'
+import { getRouterData } from './routers'
 
 // 登录相关和获取用户信息
 Mock.mock(/\/login/, login)
@@ -8,5 +9,7 @@ Mock.mock(/\/get_info/, getUserInfo)
 Mock.mock(/\/logout/, logout)
 Mock.mock(/\/get_table_data/, getTableData)
 Mock.mock(/\/get_drag_list/, getDragList)
+Mock.mock(/\/get_router/, getRouterData)
+Mock.mock(/\/getUserSysMenu/, getUserSysMenu)
 
 export default Mock
