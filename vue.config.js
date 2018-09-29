3381a23f7aeb9cb65a1fa521f7a36ce14728a449
zhang <zhangjuming2010@126.com>
2018-09-29 09:59:05 +0800

    更新官方修改“添加关闭eslint配置”

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const path = require('path')
+const path = require('path');
 
 const resolve = dir => {
   return path.join(__dirname, dir)
-}
+};
 
 // 项目部署基础
 // 默认情况下，我们假设你的应用将被部署在域的根目录下,
@@ -13,7 +13,7 @@ const resolve = dir => {
 // 需要将它改为'/my-app/'
 const BASE_URL = process.env.NODE_ENV === 'production'
   ? '/iview-admin/'
-  : '/'
+  : '/';
 
 module.exports = {
   // Project deployment base
@@ -26,6 +26,8 @@ module.exports = {
   baseUrl: BASE_URL,
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: true,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
@@ -38,4 +40,4 @@ module.exports = {
   // devServer: {
   //   proxy: 'localhost:3000'
   // }
-}
+};
